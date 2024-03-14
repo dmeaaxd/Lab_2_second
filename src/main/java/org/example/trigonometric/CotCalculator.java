677ce1a135f8a538b79d3734908e590c9a4efd28
Maksim Ushakov <dmeaaxd@gmail.com>
2024-03-14 18:59:30 +0300

    add cot/tan calc

--- src/main/java/org/example/trigonometric/CotCalculator.java
+++ src/main/java/org/example/trigonometric/CotCalculator.java
@@ -1,4 +1,17 @@
 package org.example.trigonometric;
 
 public class CotCalculator {
+    private final SinCalculator sin;
+    private final CosCalculator cos;
+
+    public CotCalculator(double precision, SinCalculator sin, CosCalculator cos) {
+        super(precision);
+        this.sin = sin;
+        this.cos = cos;
+    }
+
+    public double calculate(double x) {
+        double cot = cos.calc(x) / sin.calc(x);
+        return Double.isFinite(cot) ? cot : Double.POSITIVE_INFINITY;
+    }
 }
--- src/main/java/org/example/trigonometric/TanCalculator.java
+++ src/main/java/org/example/trigonometric/TanCalculator.java
@@ -1,4 +1,17 @@
 package org.example.trigonometric;
 
 public class TanCalculator {
+    private final SinCalculator sin;
+    private final CosCalculator cos;
+
+    public TanCalculator(double precision, SinCalculator sin, CosCalculator cos) {
+        super(precision);
+        this.sin = sin;
+        this.cos = cos;
+    }
+
+    public double calculate(double x) {
+        double tan = sin.calc(x) / cos.calc(x);
+        return Double.isFinite(tan) ? tan : Double.POSITIVE_INFINITY;
+    }
 }
